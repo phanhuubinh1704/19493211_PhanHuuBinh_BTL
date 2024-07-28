ec82fbb1cd16c6ca7621bc8c455ab9e309e68593
phanhuubinh1704 <phanhuubinh1704@gmail.com>
2024-07-28 22:07:04 +0700

    Nop bai

---
+++ ebook-main/frontend/js/logout.js
@@ -0,0 +1,28 @@
+function logout(role) {// role expected string: user, admin
+    const accessToken = window.sessionStorage.getItem(`${role}_access_token`);
+    fetch('http://localhost:8080/api/v1/auth/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+    })
+    .then((response) => {
+        window.sessionStorage.removeItem(`${role}_access_token`);
+        window.document.cookie = 'COOKIE_NAME=; Max-Age=0; path=/; domain=' + location.host;
+        switch (role) {
+            case 'user':
+                toastr.info('Thành công!', 'Đăng xuất')
+                setTimeout(() => {
+                    window.location.reload();
+                }, 500);
+                break;
+            default:
+                window.location.href = './login.html'
+                break;
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
